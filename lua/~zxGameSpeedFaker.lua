35dd1acf9330136ef56dc5289aeda0c67840cbd2
Poltergeistzx <102504102+Poltergeistzx@users.noreply.github.com>
2022-11-23 22:07:32 +0200

    Add files via upload

--- lua/~zxGameSpeedFaker.lua
+++ lua/~zxGameSpeedFaker.lua
@@ -15,7 +15,7 @@ local patchClassMethod = function(class, methodName, createPatch)
 end
 
 --patchers
-local allowedUnpause
+local allowedUnpause --maybe use integer instead of boolean for depth
 local speedPatch = function(original_fn)
     return function(...)
         if not allowedUnpause then
@@ -48,15 +48,25 @@ local fnPatch = function(obj,funcName, event)
     end
 end
 
-local function keysPatch(removedList)
-    return function(key)
-        if not MainScreen.instance:isVisible() then
+local function keyPatch( obj, fnName, event )
+    local original_fn = obj[fnName]
+    if event then
+        Events[event].Remove(original_fn)
+    end
+
+    obj[fnName] = function(...)
+        if MainScreen.instance:isVisible() then
+            return original_fn(...)
+        else
             allowedUnpause = true
-            for i,v in pairs(removedList) do
-                v[i](key)
-            end
+            local result = original_fn(...)
             allowedUnpause = false
+            return result
         end
+    end
+
+    if event then
+        Events[event].Add(obj[fnName])
     end
 end
 
@@ -67,7 +77,6 @@ isGamePaused = pausePatch(isGamePaused)
 
 --patch ui
 fnPatch(ISContextManager.getInstance(),"createWorldMenu")
-fnPatch(ISVehicleDashboard,"onClickTrunk")
 fnPatch(ISHotbar,"doMenu")
 fnPatch(ISInventoryPage,"toggleStove")
 fnPatch(ISInventoryPane,"doButtons")
@@ -77,49 +86,28 @@ fnPatch(ISVehicleDashboard,"onClickHeadlights")
 fnPatch(ISVehicleDashboard,"onClickDoors")
 fnPatch(ISVehicleDashboard,"onClickHeater")
 fnPatch(ISVehicleDashboard,"onClickKeys")
+fnPatch(ISVehicleDashboard,"onClickTrunk")
 fnPatch(ISVehicleMechanics,"onListMouseDown")
 fnPatch(ISVehicleMechanics,"doPartContextMenu")
 fnPatch(ISHealthBodyPartListBox,"onRightMouseUp")
 fnPatch(ISObjectClickHandler,"doClickSpecificObject")
 
 --patch the keys
-Events.OnGameStart.Add(function()
-    for i,v in pairs({
-        OnKeyStartPressed = {
-            onKeyPressed = ISFirearmRadialMenu,
-            onKeyStartPressed = ISHotbar,
-            onKeyPressed = ISLightSourceRadialMenu,
-            onKeyStartPressed = ISUIHandler,
-            onKeyStartPressed = ISVehicleMenu,
-            onKeyStartPressed = ISWorldMap,
-        },
-        OnKeyKeepPressed = {
-            onKeyRepeat = ISFirearmRadialMenu,
-            onKeyKeepPressed = ISHotbar,
-            onKeyRepeat = ISLightSourceRadialMenu,
-            onKeyKeepPressed = ISWorldMap,
-        },
-        OnKeyPressed = {
-            onKeyReleased = ISFirearmRadialMenu,
-            onKeyReleased = ISHotbar,
-            onKeyPressed = ISInventoryPage,
-            onKeyReleased = ISLightSourceRadialMenu,
-            handleKeyPressed = ISSearchManager,
-            onKeyPressed = ISUIHandler,
-            onKeyReleased = ISWorldMap,
-            displayCharacterInfo = xpUpdate,
-        }
-    }) do
-        for ii,vv in pairs(v) do
-            Events[i].Remove(vv[ii])
-        end
-        Events[i].Add(keysPatch(v))
-    end
-end)
+keyPatch(ISFirearmRadialMenu,"checkKey")
+keyPatch(ISHotbar,"onKeyStartPressed")
+keyPatch(ISHotbar,"onKeyPressed")
+keyPatch(ISHotbar,"onKeyKeepPressed")
+keyPatch(ISInventoryPage,"onKeyPressed","OnKeyPressed")
+keyPatch(ISLightSourceRadialMenu,"checkKey")
+keyPatch(ISSearchManager,"handleKeyPressed")
+keyPatch(ISVehicleMenu,"onShowSeatUI")
+keyPatch(ISVehicleMenu,"showRadialMenu")
+keyPatch(ISWorldMap,"checkKey")
+keyPatch(xpUpdate,"displayCharacterInfo","OnKeyPressed")
+
+
 
 --[[
 skipped: BaseIcon,SearchManager,SpeedControls,Joystick,BackButton,ButtonPrompt,ISDPadWheels,ISEmoteRadialMenu,ISLcdBar,ISSineWaveDisplay,ISTimedActionQueue,ISVehicleRegulator
 other: ISInventoryPaneContextMenu.createMenuNoItems,ISObjectClickHandler.doClick
-
-check inventory key right function
 --]]
